9c52788c15aa9a7ca6fc2550df376165e9d904f6
William Cummings <wcummings@forensiq.com>
2015-11-30 17:39:09 -0500

    Bare bones lua mode w/ correct whitespacing

---
+++ modes/lua.el
@@ -0,0 +1,10 @@
+(require 'lua-mode)
+
+(defun my-lua-mode-hook ()
+  (setq indent-tabs-mode nil)
+  (setq tab-always-indent nil)
+  (setq lua-indent-level 4))
+
+(add-hook 'lua-mode-hook 'my-lua-mode-hook)
+
+(provide 'lua)
