b46f3eb76626b7cb2f752256a592d8a21776cd9c
William Cummings <wcummings@forensiq.com>
2016-01-28 09:41:00 -0500

    Use erlang-mode indentation

--- modes/my-erlang-mode.el
+++ modes/my-erlang-mode.el
@@ -1,13 +1,12 @@
-(add-to-list 'load-path (car (file-expand-wildcards "/usr/lib/erlang/lib/tools-*/emacs")))
-(setq erlang-root-dir "/usr/lib/erlang")
-(add-to-list 'exec-path "/usr/lib/erlang/bin")
+(add-to-list 'load-path (car (file-expand-wildcards "/usr/local/lib/erlang/lib/tools-*/emacs")))
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(add-to-list 'exec-path "/usr/local/lib/erlang/bin")
 (require 'erlang-start)
 (require 'edts-start)
 
 (defun my-erlang-mode-hook ()
-  (require 'my-whitespace-mode)
-  (require 'dumb-indent)
-  (local-set-key (kbd "C-x C-r") 'find-makefile-and-run)
+  ;;(require 'my-whitespace-mode)
+  ;;(require 'dumb-indent)
   (setq whitespace-line-column 120))
 
 (add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
