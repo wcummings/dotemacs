1a97ccb2aa64eb2a056d3732a0ef042f22022314
William Cummings <wcummings@forensiq.com>
2016-02-08 00:18:54 -0500

    ac

--- modes/racket.el
+++ modes/racket.el
@@ -3,6 +3,8 @@
 (add-hook 'racket-mode-hook 'my-racket-mode-hook)
 
 (defun my-racket-mode-hook ()
+  (setq tab-always-indent 'complete)
+  (add-to-list 'completion-styles 'initials t)
   (define-key racket-mode-map (kbd "C-c r") 'racket-run)
   (flymake-mode)
   (require 'smartparens-racket))
