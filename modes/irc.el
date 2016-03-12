17af2bf4507b6059670cfd16ba9981fc374afeae
William Cummings <wcummings@forensiq.com>
2016-03-12 10:11:53 -0500

    backtick

--- modes/irc.el
+++ modes/irc.el
@@ -6,10 +6,28 @@
 
 (load "znc-secret.el")
 
-(setq rcirc-server-alist '((znc-host
-			    :port znc-port
+(setq rcirc-server-alist `((,znc-host
+			    :port ,znc-port
 			    :nick "wcummings"
-			    :password (concat "wcummings:" znc-password)
+			    :password ,(concat "wcummings:" znc-password)
 			    :full-name "wcummings")))
+
+(defun rcirc-detach-buffer ()
+  (interactive)
+  (let ((buffer (current-buffer)))
+    (when (and (rcirc-buffer-process)
+           (eq (process-status (rcirc-buffer-process)) 'open))
+      (with-rcirc-server-buffer
+    (setq rcirc-buffer-alist
+          (rassq-delete-all buffer rcirc-buffer-alist)))
+      (rcirc-update-short-buffer-names)
+      (if (rcirc-channel-p rcirc-target)
+      (rcirc-send-string (rcirc-buffer-process)
+                 (concat "DETACH " rcirc-target))))
+    (setq rcirc-target nil)
+    (kill-buffer buffer)))
+
+;; bind to C-x k so i dont accidentally part channels
+(define-key rcirc-mode-map (kbd "C-x k") 'rcirc-detach-buffer)
 
 (provide 'irc)
