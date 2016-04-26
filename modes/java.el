8c360fb63e0aa766422ac0a9f246da749a3022d8
William Cummings <wcummings@bosmm2r60n5fvh8.hubspot.local>
2016-04-26 15:37:02 -0400

    Keybinding to jump to intellij

--- modes/java.el
+++ modes/java.el
@@ -1,19 +1,7 @@
-(require 'eclim)
-(global-eclim-mode)
-(require 'eclimd)
-(require 'ac-emacs-eclim-source)
+(setq java-mode-hook
+      (lambda()
+	(local-set-key (kbd "C-x j") 'open-intellij)))
 
-(custom-set-variables
- '(eclim-eclipse-dirs '("~/eclipse"))
- '(eclim-executable "~/eclipse/eclim"))
-
-(add-hook 'java-mode-hook
-	  (lambda ()
-	    (setq help-at-pt-display-when-idle t)
-	    (setq help-at-pt-timer-delay 0.1)
-	    (help-at-pt-set-timer)
-	    (add-to-list 'ac-sources 'ac-source-emacs-eclim)
-	    (require 'autocomplete)
-	    (eclim-mode t)))
-
-(provide 'java)
+(defun open-intellij ()
+  (interactive)
+  (shell-command "open -a 'IntelliJ IDEA CE'"))
