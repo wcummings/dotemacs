c7fe0acbde76e597766aeb420756ec9fcf634948
William Cummings <wcummings@hubspot.com>
2016-08-05 12:59:33 -0400

    Switch entry to DONE when all subentries are done

--- modes/my-org-mode.el
+++ modes/my-org-mode.el
@@ -36,6 +36,15 @@
 (when my-org-mode-gcal-private-url
   (run-with-timer 0 (* 15 60) 'sync-gcal))
 
+;; http://orgmode.org/manual/Breaking-down-tasks.html
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+
 (global-set-key (kbd "C-c a a") 'org-agenda-list)
 
 (setq org-src-fontify-natively t)
