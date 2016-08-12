d743ffbc8f26f10963d2266c8029c76d927a6b57
William Cummings <wcummings@hubspot.com>
2016-08-12 15:43:00 -0400

    dont need parens

--- scripts/slack-toilet-mob.pl
+++ scripts/slack-toilet-mob.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
-system("~/.emacs.d/scripts/slack-toilet.sh $_ $ARGV[1]") foreach split(//, $ARGV[0]);
+system "~/.emacs.d/scripts/slack-toilet.sh $_ $ARGV[1]" foreach split //, $ARGV[0];
 
