d300fa8acd84771c2abcff8f7d687eb1ac232462
William Cummings <will@wpc.io>
2016-12-29 10:18:45 -0500

    sudo-edit

--- modes/my-tramp-mode.el
+++ modes/my-tramp-mode.el
@@ -6,4 +6,11 @@
 	     '((regexp-quote (system-name)) nil nil))
 (setq tramp-default-method "ssh")
 
+;; stolen from spacemacs
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'my-tramp-mode)
