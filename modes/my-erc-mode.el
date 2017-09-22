adef0766040b99f262fa6ea9c0641019c61aada8
William Cummings <will@wpc.io>
2017-09-22 16:07:28 -0400

    Patched znc.el

--- modes/my-erc-mode.el
+++ modes/my-erc-mode.el
@@ -1,48 +1,48 @@
 (require 'erc)
 (require 'tls)
+(require 'znc)
 
-(setq erc-rename-buffers t)
+;; (setq erc-rename-buffers t)
 (setq erc-interpret-mirc-color t)
 
-(defcustom my-erc-mode-servers nil
-  "ERC server config."
-  :type 'sexp
-  :group 'my-customizations)
+;; (defcustom my-erc-mode-servers nil
+;;   "ERC server config."
+;;   :type 'sexp
+;;   :group 'my-customizations)
 
-(defcustom my-erc-mode-znc-host nil
-  "ZNC host."
-  :type '(string)
-  :group 'my-customizations)
+;; (defcustom my-erc-mode-znc-host nil
+;;   "ZNC host."
+;;   :type '(string)
+;;   :group 'my-customizations)
 
-(defcustom my-erc-mode-znc-port nil
-  "ZNC port."
-  :type '(string)
-  :group 'my-customizations)
+;; (defcustom my-erc-mode-znc-port nil
+;;   "ZNC port."
+;;   :type '(string)
+;;   :group 'my-customizations)
 
-(defcustom my-erc-mode-znc-password nil
-  "ZNC password."
-  :type '(string)
-  :group 'my-customizations)
+;; (defcustom my-erc-mode-znc-password nil
+;;   "ZNC password."
+;;   :type '(string)
+;;   :group 'my-customizations)
 
-(defcustom my-erc-mode-znc-username nil
-  "ZNC username."
-  :type '(string)
-  :group 'my-customizations)
+;; (defcustom my-erc-mode-znc-username nil
+;;   "ZNC username."
+;;   :type '(string)
+;;   :group 'my-customizations)
 
-(defun znc ()
-  "Connect to a list of networks via ZNC."
-  (interactive)
-  (dolist (server my-erc-mode-servers)
-    (let* ((nick (plist-get server :nick))
-           (network (plist-get server :network))
-           (password (concat my-erc-mode-znc-username "/"
-                             network ":" my-erc-mode-znc-password)))
-      (message (format "Connecting %s" password))
-      (erc-tls :server my-erc-mode-znc-host
-           :port my-erc-mode-znc-port
-           :nick nick
-           :full-name nick
-           :password password))))
+;; (defun znc ()
+;;   "Connect to a list of networks via ZNC."
+;;   (interactive)
+;;   (dolist (server my-erc-mode-servers)
+;;     (let* ((nick (plist-get server :nick))
+;;            (network (plist-get server :network))
+;;            (password (concat my-erc-mode-znc-username "/"
+;;                              network ":" my-erc-mode-znc-password)))
+;;       (erc-tls :server my-erc-mode-znc-host
+;;            :port my-erc-mode-znc-port
+;;            :nick nick
+;;            :full-name nick
+;;            :password password))))
 
 (provide 'my-erc-mode)
 
