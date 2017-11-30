b9be3cca40b3d38d59bca76880c6853f1cb8f0ac
William Cummings <will@wpc.io>
2017-11-30 10:58:27 -0500

    Script to generate opml from rss feeds

---
+++ scripts/feeds_to_opml.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use XML::OPML;
+
+my $opml = new XML::OPML(version => "1.1");
+
+$opml->head(title => 'Subscriptions');
+
+while(<>) {
+    chomp;
+    $opml->add_outline(type => 'rss', xmlUrl => $_);
+}
+
+$opml->save('feeds.opml');
