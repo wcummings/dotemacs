f47dc7de83410da1062325502cba2b442933ba60
William Cummings <will@wpc.io>
2017-12-01 09:49:07 -0500

    Tidy up script

--- scripts/upload_to_gwene.pl
+++ scripts/upload_to_gwene.pl
@@ -5,11 +5,11 @@ my $gwene_url = "gwene.org";
 while(<>) {
     chomp;
     $feed_url = $_;
-    my $content = `curl -d "url=$feed_url&confirm=Sign Up" $gwene_url 2> /dev/null`;
-    $content =~ /group value="([^"]+)"/;
+    my $p1_content = `curl -d "url=$feed_url&confirm=Sign Up" $gwene_url 2> /dev/null`;
+    $p1_content =~ /group value="([^"]+)"/;
     my $group = $1;
-    my $content2 = `curl -d "url=$feed_url&confirm=Sign Up&group=$group" $gwene_url 2>/dev/null`;
-    if ($content2 =~ /already subscribed as ([^ ]+)/) {
+    my $p2_content = `curl -d "url=$feed_url&confirm=Sign Up&group=$group" $gwene_url 2>/dev/null`;
+    if ($p2_content =~ /already subscribed as ([^ ]+)/) {
 	print "$1\n";
     } else {
 	print "$group\n";
