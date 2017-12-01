16ee17340e955f85e9e9cddb4ceed39d70ece378
William Cummings <will@wpc.io>
2017-12-01 09:45:03 -0500

    handle already subbed case

--- scripts/upload_to_gwene.pl
+++ scripts/upload_to_gwene.pl
@@ -8,6 +8,10 @@ while(<>) {
     my $content = `curl -d "url=$feed_url&confirm=Sign Up" $gwene_url 2> /dev/null`;
     $content =~ /group value="([^"]+)"/;
     my $group = $1;
-    `curl -d "url=$feed_url&confirm=Sign Up&group=$group" $gwene_url 2>/dev/null`;
-    print "$group\n";
+    my $content2 = `curl -d "url=$feed_url&confirm=Sign Up&group=$group" $gwene_url 2>/dev/null`;
+    if ($content2 =~ /already subscribed as ([^ ]+)/) {
+	print "$1\n";
+    } else {
+	print "$group\n";
+    }
 }
